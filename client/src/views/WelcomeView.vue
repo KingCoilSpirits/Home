c83ec0c0c3ec4c54dd246468fa096c8c73b6d7d7
SJones138 <shawn@revolvesoftware.dev>
2025-02-15 22:07:44 -0600

    handling table numbers and routing to order apps

--- client/src/views/WelcomeView.vue
+++ client/src/views/WelcomeView.vue
@@ -2,6 +2,10 @@
 import {onMounted, ref, watch} from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 
+type TableUrls = {
+  [key: string]: string;
+};
+
 const route = useRoute();
 const router = useRouter();
 const showOptions = ref(false);
@@ -22,6 +26,81 @@ watch(
       tableNumber.value = getTableNumber(newTableNumber);
     }
 );
+
+const handleOrderClick = (restaurant: string, tableNumber: keyof TableUrls) => {
+  switch (restaurant) {
+    case 'ogzaza':
+      const ogUrl = `${ogBaseURL}${tableNumber}`;
+      router.push(ogUrl);
+      break;
+    case 'kingcoil':
+      const kcUrl = tableUrls[tableNumber];
+      if (kcUrl) {
+        router.push(kcUrl);
+      } else {
+        console.error('Invalid table number for King Coil');
+      }
+      break;
+    default:
+      console.error('Invalid restaurant selection');
+  }
+};
+const ogBaseURL = 'https://order.toasttab.com/order-and-pay/og-zaza-st-paul-550-vandalia-st/'
+// const kcBaseURL = 'https://cocktail-room.square.site/s/order?location=11ee3ab1d843fa70b2b23cecef6d5b2a&customer_seat_id=11efd5fba1e3d0d2a2f8f6c344c7d768'
+const tableUrls: TableUrls = {
+  "111": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0juilbsnimqfj3148",
+  "112": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0kbzypm27h49j53e0",
+  "113": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0kqzknncijsfss9k8",
+  "114": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0l4nmi6drau3dd5rc",
+  "115": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0liqskd98tcr2a8h4",
+  "116": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e7x0lvntcmp56e9p8eaw",
+  "121": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw6ix2huffeva5down",
+  "122": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw6us854epmposdlxz",
+  "123": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw705t6ndv83lo1emv",
+  "124": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw7522xb6o5n2n7g47",
+  "125": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw7al1fidip1ehawk7",
+  "126": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e8pw7ff5fo4pddkqst3r",
+  "131": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9g5cfj9ueclx1cfkodb",
+  "132": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9g5ctoln0ce9666w6lb",
+  "133": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9g5cz26ogxmu5ouxun3",
+  "134": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9g5d41pizdl2lse99pb",
+  "135": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9g5d8xz105gtbqyv5gf",
+  "211": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9utguctgzw0mynkikei",
+  "212": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9uth47ihdjljib4ovyi",
+  "213": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889e9uth8mhb3np8pil40cq",
+  "221": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ea68awrufejk8hdd07h5",
+  "222": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ea68bglkcov5sukcixnd",
+  "231": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eag902r4wmhnfnr61358",
+  "232": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eag90emaiaqaqlrsa9t8",
+  "233": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eag90jwm49k9cx0jz57w",
+  "31": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazympqyhd6hd2aqxzyv",
+  "32": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyn487egzmmlzs4wnr",
+  "33": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazynai78himxh4i1lqv",
+  "34": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyngax01ht4z7kz8lz",
+  "35": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazynltwg95lx9owqq1j",
+  "36": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazynr7haxxgi1ujrjh3",
+  "37": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazynwivetno98dykw9j",
+  "38": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyo1t7ayk6vbuzartj",
+  "39": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyo72fqfuez9jjgpxz",
+  "40": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyod1n0xwtkxzgrsrb",
+  "41": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyqkrot1q9z9q6rivr",
+  "42": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyqreojp9tsu0azcyf",
+  "43": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyqxg13zw032g5e53r",
+  "44": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyr303a558kq67zrrr",
+  "45": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889eazyr8j2uab2zkbg0biv",
+  "311": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ebikl2pssdpj48cdgbvb",
+  "312": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ebiklr87zfoe9qx47nyv",
+  "313": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ebikm2jxi799s9k5ydcn",
+  "314": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ebikmd3hu7oqst9iv11z",
+  "321": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ecdr5sgsydpkdm7d60sa",
+  "322": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ecdr63p9abbsv49trhiy",
+  "331": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ed0bpt0wlbt5j48kxoxr",
+  "332": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ed0bq5jv0i8act3aarnj",
+  "333": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ed0bqbuxwrkcggioe4xr",
+  "341": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889ednxzxe8ponxf6fqvgt7",
+  "342": "https://square.online/app/store/api/seat/2fbmzh:19dgeo6tu57r:12889edny0ofi6kbgqke2zu1n"
+};
+
 function getTableNumber(table: string | string[]): string {
   return Array.isArray(table) ? table[0] : table || '';
 }
@@ -33,8 +112,8 @@ setTimeout(() => {
 </script>
 <template class="h-screen">
   <div class="stripe-container">
-    <div class="stripe w-screen"></div> <!-- Add this div for the stripe -->
-    <div class="stripe w-screen"></div> <!-- Add this div for the stripe -->
+    <div class="stripe w-screen"></div>
+    <div class="stripe w-screen"></div>
   </div>
   <div class="q-pa-md flex flex-center h-screen">
 <div class="text-center">
@@ -62,7 +141,7 @@ setTimeout(() => {
               src="@/assets/OGZaZaLogo.png"
               alt="OG ZaZa Logo"
               class="logo raised-logo"
-              @click="router.push('/pizza')"
+              @click="handleOrderClick('ogzaza', tableNumber)"
           />
           <q-tooltip
               anchor="center right"
@@ -75,7 +154,7 @@ setTimeout(() => {
               src="@/assets/KingCoilLogoOrange.png"
               alt="King Coil Spirits Logo"
               class="KCLogo raised-image"
-              @click="router.push('/cocktails')"
+              @click="handleOrderClick('kingcoil', tableNumber)"
           />
           <q-tooltip
               anchor="center right"
